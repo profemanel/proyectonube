35370adf7c945489baab026561c48123f00113bb
manel <manel.montero@gmail.com>
2019-06-13 22:54:40 +0200

    agregamos fichero nuevo

---
+++ nuevo.sql
@@ -0,0 +1,10 @@
+este es un fichero nuevo
+vamos a subirlo a GITHUB
+
+Para ello habrá que
+
+Hacer un 
+$> git add nuevo.sql
+$> git commit -m "commit de subida de fichero nuevo"
+$> git push -u origin master
+
