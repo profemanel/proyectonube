c0eb172fc9906c4e25634c5bf5697427a59daedf
manel <manel.montero@gmail.com>
2019-06-13 22:37:00 +0200

    commit inicial

---
+++ fichero1.sql
@@ -0,0 +1,6 @@
+-- este es el fichero1.sql
+
+-- hacemos cambios en la linea 3
+-- hacemos cambios en la linea 4
+
+-- HACEMOS CAMBIOS EN LA LINEA 5
---
+++ fichero3.sql
@@ -0,0 +1,8 @@
+-- este es el fichero3.sql
+
+-- este fichero haria un insert
+
+insert into alumnos (idalumno, nombre) values (1, 'pepe');
+
+
+-- fin del fichero 3
